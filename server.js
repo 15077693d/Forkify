d12e08b479fd617b7bf6856aa4fc482285320dfe
15077693d <15077693d@gmail.com>
2020-05-27 00:43:01 +0800

    update server

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
-/* eslint-disable */
-var express = require('express');
-var path = require('path');
-
-var port = process.env.PORT || 3000;
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+// server.js
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
-
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`server started port: ${port}`);
-  }
-});
+app.listen(port);
